3b7decb669bb14d1ae558d13ffc1b97a78f190b2
ElProfeCoss <44418987+ElProfeCoss@users.noreply.github.com>
2020-03-22 15:25:31 -0600

    Add files via upload

---
+++ apuntes01000.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html leng='es'>	
+	<head>			
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="description" content="">
+		<meta name="keywords" content="">
+		<meta name="author" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title></title>
+	</head>
+	<body>
+	</body>
+</html>		
+
